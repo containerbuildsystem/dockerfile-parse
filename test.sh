0175bf685b83edce2d6d6afc5d43866c0b76aba6
Luiz Carvalho <lucarval@redhat.com>
2017-12-19 09:00:50 -0500

    Fix travis testing

--- test.sh
+++ test.sh
@@ -15,13 +15,12 @@ fi
 CONTAINER_NAME="dockerfile-parse-$OS-$OS_VERSION-py$PYTHON_VERSION"
 RUN="docker exec -ti $CONTAINER_NAME"
 if [[ $OS == "fedora" ]]; then
-  if [[ $OS_VERSION == 25 && $PYTHON_VERSION == 2 ]]; then PIP_PKG="python-pip"; else PIP_PKG="python$PYTHON_VERSION-pip"; fi
+  PIP_PKG="python$PYTHON_VERSION-pip"
   PIP="pip$PYTHON_VERSION"
   PKG="dnf"
   PKG_EXTRA="dnf-plugins-core"
   BUILDDEP="dnf builddep"
   PYTHON="python$PYTHON_VERSION"
-  PYTEST="py.test-$PYTHON_VERSION"
 else
   PIP_PKG="python-pip"
   PIP="pip"
@@ -29,7 +28,6 @@ else
   PKG_EXTRA="yum-utils epel-release"
   BUILDDEP="yum-builddep"
   PYTHON="python"
-  PYTEST="py.test"
 fi
 # Create container if needed
 if [[ $(docker ps -q -f name=$CONTAINER_NAME | wc -l) -eq 0 ]]; then
@@ -41,12 +39,12 @@ $RUN $PKG install -y $PKG_EXTRA
 $RUN $BUILDDEP -y python-dockerfile-parse.spec
 if [[ $OS != "fedora" ]]; then
   # Install dependecies for test, as check is disabled for rhel
-  $RUN yum install -y python-flexmock python-six
+  $RUN yum install -y python-six
 fi
 
 # Install package
 $RUN $PKG install -y $PIP_PKG
-if [[ $PYTHON_VERSION == 3 && $OS_VERSION == rawhide ]]; then
+if [[ $PYTHON_VERSION == 3 ]]; then
   # https://fedoraproject.org/wiki/Changes/Making_sudo_pip_safe
   $RUN mkdir -p /usr/local/lib/python3.6/site-packages/
 fi
@@ -54,9 +52,10 @@ $RUN $PYTHON setup.py install
 
 # Install packages for tests
 $RUN $PIP install -r tests/requirements.txt
+
+# CentOS needs to have setuptools updates to make pytest-cov work
+if [[ $OS != "fedora" ]]; then $RUN $PIP install -U setuptools; fi
 if [[ $PYTHON_VERSION -gt 2 ]]; then $RUN $PIP install -r requirements-py3.txt; fi
 
 # Run tests
-$RUN $PIP install pytest-cov
-if [[ $OS != "fedora" ]]; then $RUN $PIP install -U pytest-cov; fi
-$RUN $PYTEST -vv tests --cov dockerfile_parse "$@"
+$RUN py.test -vv tests --cov dockerfile_parse "$@"
